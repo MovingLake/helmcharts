ae8228e917990386c7f5e8ed1f1395ac7c1f0cf9
Edgar Cabrera <edgar@movinglake.com>
2022-09-15 21:57:18 +0000

    Add pending cr command

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -9,3 +9,6 @@ chmod +x /usr/local/bin/kubectx
 wget https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubens
 sudo mv kubens /usr/local/bin/kubens
 chmod +x /usr/local/bin/kubens
+
+curl -sSL https://github.com/helm/chart-releaser/releases/download/v0.2.1/chart-releaser_0.2.1_linux_amd64.tar.gz | tar xzf -
+sudo mv cr /usr/local/bin/cr
